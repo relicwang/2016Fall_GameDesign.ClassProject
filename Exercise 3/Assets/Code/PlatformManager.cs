ac12a62bebfa9dfef76043612eecb402e0afe939
Yongfu(Relic) Wang <yongfuwang2018@u.northwestern.edu>
2017-01-25 22:15:10 -0600

    Add files via upload

---
+++ Exercise 3/Assets/Code/PlatformManager.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using System.Collections.Generic;   // not just System.Collections
+using System;
+
+/// <summary>
+/// Maintains a set of platforms and places then as they're needed.
+/// 
+/// IMPORTANT: this must be placed in the player character,
+/// not in its own object.
+/// </summary>
+[RequireComponent(typeof(Runner))]
+public class PlatformManager : MonoBehaviour {
+    #region Parameters editable in Unity
+    /// <summary>
+    /// Prefab to use for creating new platforms
+    /// </summary>
+    public GameObject PlatformPrefab;
+
+    /// <summary>
+    /// Spawn a new platform when the last platform is this far ahead of the player.
+    /// </summary>
+    public float SpawnAtDistance = 50f;
+    
+    // Platforms are created with random width in the range [MinPlatformWidth, MaxPlatformWidth]
+    public float MinPlatformWidth = 20f;
+    public float MaxPlatformWidth = 30f;
+
+    // Platforms are spawned with a horizontal separation in the range [MinXSpacing, MaxXSpacing]
+    public float MinXSpacing = 35f;
+    public float MaxXSpacing = 45f;
+
+    // Platforms are spawned with a vertical spacing in the range [MinYSpacing, MaxYSpacing]
+    public float MinYSpacing = -8f;
+    public float MaxYSpacing = 8f;
+
+    /// <summary>
+    /// Platforms will be recycled when the player is this distance past them.
+    /// </summary>
+    public float RecycleDistance = 50f;//50
+    #endregion
+
+    #region Internal state of the platform pool
+    /// <summary>
+    /// Location at which the next platform will be spawned
+    /// </summary>
+    private Vector3 nextSpawnPoint = new Vector3 (0f, -6.5f, 0f);
+
+    /// <summary>
+    /// The platforms that are currently on screen.
+    /// </summary>
+    private readonly Queue<GameObject> platformsInUse = new Queue<GameObject> ();
+    /// <summary>
+    /// A pool of platforms that have been instantiated in the past but that aren't
+    /// currently needed.  We save these, rather than destroying them so that we can
+    /// avoid destorying and recreating platforms, which is somewhat expensive.
+    /// </summary>
+    private readonly Queue<GameObject> unusedPlatforms = new Queue<GameObject> ();
+    #endregion
+
+    /// <summary>
+    /// True when the player hasn't lost yet, i.e. when the game isn't over.
+    /// </summary>
+    private bool playerNotDead = true;
+
+    /// <summary>
+    /// Initialize
+    /// </summary>
+    internal void Start(){
+        FindObjectOfType<Runner>().FellIntoTheVoid += OnGameOver;
+        SpawnNewPlatform();
+    }
+
+    /// <summary>
+    /// Handling platform generating and recycling.
+    /// </summary>
+    internal void Update()
+    {
+        if (playerNotDead) {
+            if (ShouldRecyclePlatform(OldestPlatformInUse, gameObject))
+            {
+                RecycleOldestPlatform();
+            }
+
+            if (ShouldSpawnPlatform(nextSpawnPoint, gameObject))
+            {
+                SpawnNewPlatform();
+            }
+        }    
+    }
+
+    /// <summary>
+    /// Check whether the platform should be spawned.
+    /// </summary>
+    /// <param name="spawnPoint"></param>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    private bool ShouldSpawnPlatform(Vector3 spawnPoint, GameObject player)
+    {
+        float playPosX = player.transform.position.x;
+        float spawnPointX = spawnPoint.x;
+
+        if ((spawnPointX - playPosX) <= SpawnAtDistance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///  Check whether the platform should be recycled.
+    /// </summary>
+    /// <param name="oldestPlatformInUse"></param>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    private bool ShouldRecyclePlatform(GameObject oldestPlatformInUse, GameObject player)
+    {
+        float playPosX = player.transform.position.x;
+        float olatformPosX = oldestPlatformInUse.transform.position.x;
+
+        if ((playPosX- olatformPosX) >= RecycleDistance) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Returns the platform that's been on screen for the longest time,
+    /// aka the rearmost platform that's on screen.
+    /// </summary>
+    private GameObject OldestPlatformInUse
+    {
+        get { return platformsInUse.Peek(); }
+    }
+
+    /// <summary>
+    /// Recycle oldest platform out of scene.
+    /// </summary>
+    private void RecycleOldestPlatform()
+    {
+        GameObject platform;
+        if (platformsInUse.Count > 0)
+        {
+            platform = platformsInUse.Dequeue();
+            platform.SetActive(false);
+
+            unusedPlatforms.Enqueue(platform);
+        }
+ 
+    }
+    /// <summary>
+    /// Remove an unused platform from the pool, place it, and activate it.
+    /// </summary>
+    private void SpawnNewPlatform() {
+        GameObject platform;
+        if (unusedPlatforms.Count > 0) {
+            platform = unusedPlatforms.Dequeue();
+            platform.SetActive(true);
+        } else {
+            platform = Instantiate(PlatformPrefab);
+        }
+
+        platformsInUse.Enqueue(platform);
+        MovePlatformToSpawnPoint(platform);
+    }
+
+    /// <summary>
+    /// Move platform to SpawnPoint
+    /// </summary>
+    /// <param name="platform"></param>
+    private void MovePlatformToSpawnPoint(GameObject platform)
+    {
+        platform.transform.position = nextSpawnPoint;
+        platform.transform.localScale =
+            new Vector3(
+                UnityEngine.Random.Range(MinPlatformWidth, MaxPlatformWidth),
+                1f,
+                1f);
+
+        nextSpawnPoint.x += UnityEngine.Random.Range
+            (MinXSpacing, MaxXSpacing);
+
+        nextSpawnPoint.y += UnityEngine.Random.Range
+     (MinYSpacing, MaxYSpacing);
+    }
+
+    /// <summary>
+    /// Called when the game is over.
+    /// </summary>
+    private void OnGameOver(){
+        // Clear playerNotDead
+        playerNotDead = false;
+    }
+}
---
+++ Exercise 3/Assets/Code/Runner.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System;
+
+/// <summary>
+/// Controller for the player
+/// </summary>
+public class Runner : MonoBehaviour
+{
+    #region Fields that can be adjusted in the editor
+    /// <summary>
+    /// Rate at which character accelerates horizontally when running
+    /// </summary>
+    public float RunningAccelerationRate = 5f;
+    /// <summary>
+    /// Speed character at which rises when jumping
+    /// </summary>
+    public float JumpSpeed = 20f;
+    /// <summary>
+    /// Speed at which character falls
+    /// (platformers don't usually implement acceleration due to gravity)
+    /// </summary>
+    public float Gravity = 0.5f;
+    /// <summary>
+    /// If the player drops beneath this Y coordinate, they're considered to have fallen into the void.
+    /// The right way to do this is to compute the Y coordinate of the bottom of the screen, but we
+    /// haven't shown you how to do that yet.
+    /// </summary>
+    public int BottomOfTheWorld = -60;
+    #endregion
+
+    #region Event notifications for other components
+    /// <summary>
+    /// Called when the character land on a platform
+    /// </summary>
+    public event Action LandedOnPlatform = delegate { };
+    /// <summary>
+    /// Called when the character falls into the void
+    /// </summary>
+    public event Action FellIntoTheVoid = delegate { };
+    /// <summary>
+    /// True if we've already called FellIntoTheVoid.
+    /// </summary>
+    private bool notifiedGameEnd;
+    #endregion
+
+    #region Internal state variables
+    /// <summary>
+    /// Kluge factor for aligning character with platform when they land.
+    /// Its value is determined by the sizes of the character and the platform and where
+    /// the origins of their coordinate systems are relative to their collider edges.
+    /// Someday, this should get refactored to something cleaner.
+    /// </summary>
+    private float magicHeightOffset;
+
+    /// <summary>
+    /// Player's current velocity
+    /// </summary>
+    private Vector3 velocity;
+
+    /// <summary>
+    /// True when player is on a platform
+    /// </summary>
+    private bool isTouchingPlatform;
+
+    /// <summary>
+    /// Iniitialization
+    /// </summary>
+    internal void Awake()
+    {
+        magicHeightOffset = (GetComponent<BoxCollider2D>().size.y + 1f) / 2f;
+        velocity = new Vector3(10f, 0f, 0f);
+      //  notifiedGameEnd = false;
+        //isTouchingPlatform = true;
+    }
+    #endregion
+
+    /// <summary>
+    /// Update player's status by input
+    /// </summary>
+    internal void Update()
+    {
+    
+        if (isTouchingPlatform)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = JumpSpeed;
+            }
+
+            if (Input.GetButtonDown("Short Jump"))
+            {
+                velocity.y = (JumpSpeed / 2f);
+            }
+            velocity.x = velocity.x + (RunningAccelerationRate * Time.deltaTime);
+        }
+
+        else
+        {
+            velocity.y -= (Gravity * Time.deltaTime);
+        }
+
+        transform.Translate(velocity.x * Time.deltaTime, velocity.y * Time.deltaTime,0);
+
+        if (gameObject.transform.position.y <= BottomOfTheWorld)
+        {
+            if (!notifiedGameEnd)
+            {
+                FellIntoTheVoid.Invoke();
+                notifiedGameEnd = true;
+            }
+        }
+        if (gameObject.transform.position.y <= (BottomOfTheWorld * 2))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+
+    /// <summary>
+    /// Snap character to the top of the platform at the specified position.
+    /// We use this so that if a character hits the platform from the side, they
+    /// "climb up" the platform.  It makes the game somewhat easier.
+    /// </summary>
+    /// <param name="platformPosition"></param>
+    private void AlignToPlatform(Vector3 platformPosition)
+    {
+        var myPos = transform.position;
+        myPos.y = platformPosition.y + magicHeightOffset;
+        transform.position = myPos;
+    }
+
+    /// <summary>
+    /// Event handling when player touch the platform
+    /// </summary>
+    /// <param name="other"></param>
+    internal void OnTriggerEnter2D(Collider2D other)
+    {
+        isTouchingPlatform = true;
+        LandedOnPlatform.Invoke();
+        AlignToPlatform(other.transform.position);
+        velocity.y = 0f;
+    }
+
+    /// <summary>
+    /// Event handling when player leaves the platform
+    /// </summary>
+    /// <param name="target"></param>
+    internal void OnTriggerExit2D(Collider2D target)
+    {
+        isTouchingPlatform = false;
+    }
+}
