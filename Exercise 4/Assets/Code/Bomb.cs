5d007f4689e8f0f7433fe9ea29ce000746b6be90
Yongfu(Relic) Wang <yongfuwang2018@u.northwestern.edu>
2017-01-25 22:26:33 -0600

    Add files via upload

---
+++ Exercise 4/Assets/Code/Bomb.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    /// <summary>
+    /// Threshold to trigger explosion
+    /// </summary>
+    public float ThresholdForce = 2;
+
+    public GameObject ExplosionPrefab;
+
+    /// <summary>
+    /// Destroys the box
+    /// </summary>
+    private void Destruct()
+    {
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// 1. Turn on the PointEffector2D
+    /// 2. Turning off the box’s SpriteRenderer
+    /// </summary>
+    private void Boom()
+    {
+        gameObject.GetComponent<PointEffector2D>().enabled = true;
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+        Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+
+        //Destruct() occur in 0.1 seconds
+        Invoke("Destruct", 0.1f);
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        //https://docs.unity3d.com/ScriptReference/Collision-relativeVelocity.html
+        if (obj.relativeVelocity.magnitude>= ThresholdForce)
+        {
+            Boom();
+        }
+    }
+
+
+}
---
+++ Exercise 4/Assets/Code/TargetBox.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class TargetBox : MonoBehaviour
+{
+    /// <summary>
+    /// Targets that move past this point score automatically.
+    /// </summary>
+    public static float OffScreen;
+
+    /// <summary>
+    /// Ensure AddToScore() only fired once
+    /// </summary>
+    bool isScored = false;
+
+    internal void Start() {
+        OffScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-100, 0, 0)).x;
+    }
+
+    internal void Update()
+    {
+        if (transform.position.x > OffScreen)
+            Scored();
+    }
+
+    /// <summary>
+    /// Turn the target box green, and passing the mass of the targetbox to ScoreKeeper
+    /// </summary>
+    private void Scored()
+    {      
+        gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        if (!isScored)
+        {
+            ScoreKeeper.AddToScore((int)gameObject.GetComponent<Rigidbody2D>().mass);
+            isScored = true;    
+        }
+    }
+
+    /// <summary>
+    /// When the projectile hits the ground, call the Scored() procedure 
+    /// </summary>
+    /// <param name="obj"></param>
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Ground")
+        {
+            Scored();
+        }
+    }
+}
